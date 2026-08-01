656b670502de98d9ddb439bd17fbf64554b4dfdc
Zelim <Zelimus@gmail.com>
2026-08-02 00:04:42 +0200

    fix: improve mobile small-screen scaling

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "tibiago-static-v28";
-const CLIENT_BUILD = "20260801.2";
+const CACHE_NAME = "tibiago-static-v29";
+const CLIENT_BUILD = "20260801.3";
 const APP_SHELL = [
   "/manifest.webmanifest",
   "/png/pwa-icon-192.png",
